cba0104249d330d339612217527e935f437c600e
kt-321 <kt.shzk247@gmail.com>
2019-09-24 10:03:24 +0000

    change AdminTest

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -5,11 +5,8 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-<<<<<<< HEAD
-=======
 use Illuminate\Http\Response;
 
->>>>>>> 87900444ae2cc6a77883d0bf3ad040c59c2cceef
 
 use App\User;
 use App\Song;
@@ -22,43 +19,7 @@ class AdminTest extends TestCase
      * @return void
      */
      
-<<<<<<< HEAD
     use RefreshDatabase;
-     
-    // public function test_admin_user_can_manage_songs()
-    // {   
-    //     // 管理者権限を持ったユーザーを１人作成する
-    //     $user1 = factory(User::class)->create([
-    //         "role" => 5,
-    //     ]);
-        
-    //     // 管理者権限を持たないユーザーを１人作成する
-    //     $user2 = factory(User::class)->create();
-        
-    //     // ユーザーはランダムで、曲を１曲投稿する
-    //     $song = factory(Song::class)->create();
-        
-    //     // 管理者用の曲管理画面に移動する
-    //     $response = $this->actingAs($user1)->get("index-for-admin");
-    //     $response->assertStatus(200);
-        
-    //     // 管理者権限のないユーザーでは曲管理画面を表示できないことを確認する
-    //     $response = $this->actingAs($user2)->get("index-for-admin");
-    //     $response->assertStatus(302);
-        
-    //     // 作成した曲を削除する
-    //     $response = $this->actingAs($user1)->get("delete/{$song->id}");
-    //     $response->assertRedirect("index-for-admin");
-        
-    //     // 削除した曲を戻す
-    //     $response = $this->actingAs($user1)->get("restore/{$song->id}");
-    //     $response->assertRedirect("index-for-admin");
-    // }
-}
-=======
-     use RefreshDatabase;
-     
-    //  protected function setUp(): void
     // {
     //     parent::setUp();
 
@@ -236,4 +197,3 @@ class AdminTest extends TestCase
         $response->assertStatus(403);
     }
 }
->>>>>>> 87900444ae2cc6a77883d0bf3ad040c59c2cceef
