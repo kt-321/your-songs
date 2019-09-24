c5396864f2dd06906f58d880984526334b795672
kt-321 <kt.shzk247@gmail.com>
2019-09-24 08:56:21 +0000

    circleci

--- config/database.php
+++ config/database.php
@@ -78,7 +78,7 @@ return [
             'prefix' => '',
         ],
         
-        'circleci' => [
+        'circle_test' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
